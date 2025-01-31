8fe4da17ae5772fd978ee22081731bcced0bb95f
Gospod9boga <didovecv@mail.ru>
2025-01-31 20:38:23 +0300

    Удалены неиспользуемые импорты в SubTaskTest.java

--- src/test/SubTaskTest.java
+++ src/test/SubTaskTest.java
@@ -3,12 +3,10 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import manager.InMemoryTaskManager;
-import manager.TaskManager;
-import manager.TaskManager;
+
 import task.Epic;
 import task.SubTask;
-import task.Task;
-import manager.Managers;
+
 import task.TaskStatus;
 import org.junit.jupiter.api.BeforeEach;
 
