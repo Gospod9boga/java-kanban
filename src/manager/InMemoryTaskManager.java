fc123467010a1eab90f4616d87b489704a88decd
Gospod9boga <didovecv@mail.ru>
2025-08-08 17:35:45 +0300

    Fix Checkstyle: remove trailing whitespace in InMemoryTaskManager

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -17,7 +17,7 @@ public class InMemoryTaskManager implements TaskManager {
     protected HashMap<Integer, Epic> epics = new HashMap<>();
     private HistoryManager historyManager = Managers.getDefaultHistory();
     private int nextId = 1;
-    
+
     public int getNextId() {
         return nextId;
     }
